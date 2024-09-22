6c1984450d0c4f1cf7da47626f1d7ed068d4ab5b
maruthionline <76559448+maruthionline@users.noreply.github.com>
2024-09-22 23:21:50 +0530

    Update find_duplicates.py

--- find_duplicates.py
+++ find_duplicates.py
@@ -1,15 +1,18 @@
 
-def find_duplicates(numbers_list) :
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+
+def find_duplicate(numbers_list) :
     i=0
     j=0
-    dulicates =[]
+    duplicates =[]
     for i in range (len(numbers_list)):
         for j in range (len(numbers_list)):
-            if (i!=j and numbers_list[i]==numbers_list[j] and numbers_list[i] not in dulicates) :
-                dulicates.append(numbers_list[i])
+            if (i!=j and numbers_list[i]==numbers_list[j] and numbers_list[i] not in duplicates) :
+                duplicates.append(numbers_list[i])
                 break
         
-    print (dulicates)
+    print (duplicates)
     
 numbers_list = [1, -1, 0,2, 2,4,3,3, 4, 5,0,-1,2,0,5]
-find_duplicates(numbers_list)
+find_duplicate(numbers_list)
